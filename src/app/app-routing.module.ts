b408e8d78add8d1525958a020b48b833ceb5b320
DanielISC <34218941+DanielISC@users.noreply.github.com>
2019-08-22 16:37:24 -0500

    Add adminRoutes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,47 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LandingPageComponent } from '../app/landing-page/landing-page.component';
 import { SkillsTreeComponent } from '../app/skills-tree/skills-tree.component';
+import { AdminComponent } from './admin/admin.component';
+import { AdminTableComponent } from './admin/admin-table/admin-table.component';
+import { AdminCrudComponent } from './admin/admin-crud/admin-crud.component';
+import { TypeDataResolverService, TechnologyResolverService, TopicResolverService, ReferenceResolverService } from './resolvers/admin-resolver.service';
 
 const routes: Routes = [
   {path: 'home', component: LandingPageComponent},
   {path: 'tree', component: SkillsTreeComponent},
   {path: '', redirectTo: '/home', pathMatch: 'full'},
+  {path: 'admin', component: AdminComponent,
+    children: [
+      {path: '', redirectTo: ':type/table', pathMatch: 'full'},
+      {
+        path: ':type/table',
+        component: AdminTableComponent,
+        resolve: {
+          data: TypeDataResolverService       
+        }
+      },
+      {
+        path: ':type/create',
+        component: AdminCrudComponent,
+        resolve: {
+          dataTechnology: TechnologyResolverService,
+          dataTopic: TopicResolverService,
+          dataReference: ReferenceResolverService,
+          type: TypeDataResolverService
+        }
+      },
+      {
+        path: ':type/edit/:id',
+        component: AdminCrudComponent,
+        resolve: {
+          dataTechnology: TechnologyResolverService,
+          dataTopic: TopicResolverService,
+          dataReference: ReferenceResolverService,
+          type: TypeDataResolverService
+        }
+      },
+    ]
+  }
 ];
 
 @NgModule({
